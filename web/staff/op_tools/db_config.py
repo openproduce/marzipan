959ad32665212badc6c36e39b70d32896b0db4f0
Steven Lucy <slucy@parallactic.com>
2021-01-14 18:23:57 -0600

    handle failed connections to fatt merchant server without crashing

--- web/staff/op_tools/db_config.py
+++ web/staff/op_tools/db_config.py
@@ -1,13 +1,13 @@
 register_tape_db_config = {
-        "db_user" : "marzipan",
-        "db_pass" : "",
+        "db_user" : "root",
+        "db_pass" : "test",
         "db_host" : "localhost",
         "db_port" : 3306,
         "db_name" : "register_tape"
 } 
 inventory_db_config = {
-        "db_user" : "marzipan",
-        "db_pass" : "",
+        "db_user" : "root",
+        "db_pass" : "test",
         "db_host" : "localhost",
         "db_port" : 3306,
         "db_name" : "inventory"
